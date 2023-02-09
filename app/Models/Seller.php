c13b24474ae86000508d1f36789701b3f9b7ccdb
welberthenrique <welberthenrique@gmail.com>
2023-02-09 15:52:20 -0300

    Fix: correção nos Models. Adição do SofDeletes

--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Seller extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'company_id',
@@ -29,6 +30,6 @@ class Seller extends Model
 
     public function sales(): HasMany
     {
-        return $this->hasMany(Seller::class);
+        return $this->hasMany(Sale::class);
     }
 }
